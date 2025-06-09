0673ecb96e3b2112407dc4c6447a78f4354e93d6
Ang Kim Joo <kimjoo2104@gmail.com>
2025-06-09 14:34:27 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,14 +5,13 @@ import { getDatabase, type Database } from "firebase/database";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBR-Bjxx8tw3kOrvDB5TCi_mQcX9bBci_w",
-  authDomain: "weather-station-25716.firebaseapp.com",
-  databaseURL: "https://weather-station-25716-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "weather-station-25716",
-  storageBucket: "weather-station-25716.appspot.com",
-  messagingSenderId: "913577606977",
-  appId: "1:913577606977:web:d435002668462e212aabc0",
-  measurementId: "G-H2JTR796JD"
+  apiKey: "AIzaSyC5ln8OWl3oJCC7ZBFO3kXidxucmNEAHC8",
+  authDomain: "weatherview-88av2.firebaseapp.com",
+  projectId: "weatherview-88av2",
+  storageBucket: "weatherview-88av2.firebasestorage.app",
+  messagingSenderId: "323164095317",
+  appId: "1:323164095317:web:e3d2ce70557914cccc65dc",
+  databaseURL: "https://weather-station-25716-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
 
 // Initialize Firebase
